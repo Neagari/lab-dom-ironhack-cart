c5c17ecc0bffb188ee148618dbbc7b7d9b1bed7a
“Neagari” <rodrigo.raimondi8@gmail.com>
2023-01-18 21:27:34 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  
+let priceElement = document.querySelector('.price span').innerHTML;
+console.log(priceElement)
 
-  //... your code goes here
+let quantityElement = document.querySelector('.quantity input').value;
+console.log(quantityElement)
+
+console.log('Calculating subtotal, yey!')
+
+
+let subTotal = priceElement * quantityElement;
+
+let resultSubTotal = product.querySelector('.subtotal span')
+resultSubTotal.innerText = subTotal;
+
+return subTotal;
+
+
 }
-
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
@@ -14,9 +28,8 @@ function calculateAll() {
   // end of test
 
   // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
+ 
+  // ITERATION 3.in
   //... your code goes here
 }
 
